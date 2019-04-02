568f6598be85338b61d9b276cc9ddb44a5546925
Muhammed Kartal <48036846+MuhammedRKartal@users.noreply.github.com>
2019-04-02 18:28:30 +0300

    Update saveOperations.java

--- GUIDemo/saveOperations.java
+++ GUIDemo/saveOperations.java
@@ -1,47 +1,73 @@
-package GUIDemo;
+package Project.GUIDemo;
 
+import Project.mainObjects.File_f;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.pdf.PdfWriter;
-import mainObjects.File_f;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.DirectoryChooser;
 import org.knowm.xchart.BitmapEncoder;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class saveOperations {
+    public static String currentPath = "";
+
     public static void saveToPDF(File_f file) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText(null);
+        alert.setContentText("Do you want to select save directory?");
+        alert.setTitle("Question");
+        alert.showAndWait();
+
+        Path currentRelativePath = Paths.get("");
+        if (alert.getResult() == ButtonType.OK){
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            directoryChooser.setTitle("Select Path");
+            File directory = directoryChooser.showDialog(null);
+            currentRelativePath = Paths.get(directory.getPath());
+        }
+
+
+
+        currentPath= currentRelativePath.toAbsolutePath().toString();
+        System.out.println(currentPath);
 
 //        if (usableArray.size()==1){
-        BitmapEncoder.saveBitmap(Charts.createCevaplamaOraniPieChart(file), "C:/Users/hiroh/Desktop/Try/chart1singlefile.png", BitmapEncoder.BitmapFormat.PNG);
-        BitmapEncoder.saveBitmap(Charts.createSectionOrtalamariPieChart(file), "C:/Users/hiroh/Desktop/Try/chart2singlefile.png", BitmapEncoder.BitmapFormat.PNG);
-        BitmapEncoder.saveBitmap(Charts.createScoringForAllSectionsPieChart(file), "C:/Users/hiroh/Desktop/Try/chart3singlefile.png", BitmapEncoder.BitmapFormat.PNG);
+        BitmapEncoder.saveBitmap(Charts.createCevaplamaOraniPieChart(file), currentPath+"\\chart1singlefile.png", BitmapEncoder.BitmapFormat.PNG);
+        BitmapEncoder.saveBitmap(Charts.createSectionOrtalamariPieChart(file), currentPath+"\\chart2singlefile.png", BitmapEncoder.BitmapFormat.PNG);
+        BitmapEncoder.saveBitmap(Charts.createScoringForAllSectionsPieChart(file), currentPath+"\\chart3singlefile.png", BitmapEncoder.BitmapFormat.PNG);
 
-        BitmapEncoder.saveBitmap(Charts.createScoringForAllSectionsBarChart(file), "C:/Users/hiroh/Desktop/Try/chart4singlefile.png", BitmapEncoder.BitmapFormat.PNG);
-        BitmapEncoder.saveBitmap(Charts.createMultipleAveragesBarChart(file), "C:/Users/hiroh/Desktop/Try/chart5singlefile.png", BitmapEncoder.BitmapFormat.PNG);
-        BitmapEncoder.saveBitmap(Charts.createScoringOfLearningOutComesBarChart(file), "C:/Users/hiroh/Desktop/Try/chart6singlefile.png", BitmapEncoder.BitmapFormat.PNG);
-        BitmapEncoder.saveBitmap(Charts.createSectionOrtalamalariBarChart(file), "C:/Users/hiroh/Desktop/Try/chart7singlefile.png", BitmapEncoder.BitmapFormat.PNG);
+        BitmapEncoder.saveBitmap(Charts.createScoringForAllSectionsBarChart(file), currentPath+"\\chart4singlefile.png", BitmapEncoder.BitmapFormat.PNG);
+        BitmapEncoder.saveBitmap(Charts.createMultipleAveragesBarChart(file), currentPath+"\\chart5singlefile.png", BitmapEncoder.BitmapFormat.PNG);
+        BitmapEncoder.saveBitmap(Charts.createScoringOfLearningOutComesBarChart(file), currentPath+"\\chart6singlefile.png", BitmapEncoder.BitmapFormat.PNG);
+        BitmapEncoder.saveBitmap(Charts.createSectionOrtalamalariBarChart(file), currentPath+"\\chart7singlefile.png", BitmapEncoder.BitmapFormat.PNG);
 
-        BitmapEncoder.saveBitmap(Charts.createMultipleAveragesRadarChart(file), "C:/Users/hiroh/Desktop/Try/chart8singlefile.png", BitmapEncoder.BitmapFormat.PNG);
-        BitmapEncoder.saveBitmap(Charts.createSectionOrtalamalariRadarChart(file),"C:/Users/hiroh/Desktop/Try/chart9singlefile.png", BitmapEncoder.BitmapFormat.PNG);
-        BitmapEncoder.saveBitmap(Charts.createScoringOfLearningOutComesRadarChart(file),"C:/Users/hiroh/Desktop/Try/chart10singlefile.png", BitmapEncoder.BitmapFormat.PNG);
+        BitmapEncoder.saveBitmap(Charts.createMultipleAveragesRadarChart(file), currentPath+"\\chart8singlefile.png", BitmapEncoder.BitmapFormat.PNG);
+        BitmapEncoder.saveBitmap(Charts.createSectionOrtalamalariRadarChart(file),currentPath+"\\chart9singlefile.png", BitmapEncoder.BitmapFormat.PNG);
+        BitmapEncoder.saveBitmap(Charts.createScoringOfLearningOutComesRadarChart(file),currentPath+"\\chart10singlefile.png", BitmapEncoder.BitmapFormat.PNG);
 
 
 
 
         Document document = new Document();
 
-        String input1 = "C:/Users/hiroh/Desktop/Try/chart1singlefile.png"; // .gif and .jpg are ok too!
-        String input2 = "C:/Users/hiroh/Desktop/Try/chart2singlefile.png";
-        String input3 = "C:/Users/hiroh/Desktop/Try/chart3singlefile.png";
-        String input4 = "C:/Users/hiroh/Desktop/Try/chart4singlefile.png";
-        String input5 = "C:/Users/hiroh/Desktop/Try/chart5singlefile.png";
-        String input6 = "C:/Users/hiroh/Desktop/Try/chart6singlefile.png";
-        String input7 = "C:/Users/hiroh/Desktop/Try/chart7singlefile.png";
-        String input8 = "C:/Users/hiroh/Desktop/Try/chart8singlefile.png";
-        String input9 = "C:/Users/hiroh/Desktop/Try/chart9singlefile.png";
-        String input10 = "C:/Users/hiroh/Desktop/Try/chart10singlefile.png";
+        String input1 = currentPath+"\\chart1singlefile.png"; // .gif and .jpg are ok too!
+        String input2 = currentPath+"\\chart2singlefile.png";
+        String input3 = currentPath+"\\chart3singlefile.png";
+        String input4 = currentPath+"\\chart4singlefile.png";
+        String input5 = currentPath+"\\chart5singlefile.png";
+        String input6 = currentPath+"\\chart6singlefile.png";
+        String input7 = currentPath+"\\chart7singlefile.png";
+        String input8 = currentPath+"\\chart8singlefile.png";
+        String input9 = currentPath+"\\chart9singlefile.png";
+        String input10 = currentPath+"\\chart10singlefile.png";
 
-        String output = "C:/Users/hiroh/Desktop/Try/outputs.pdf";
+        String output = currentPath+"\\outputs.pdf";
 
         try {
             FileOutputStream fos = new FileOutputStream(output);
